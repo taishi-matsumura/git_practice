2e43280068b5a0a42fb696ca7f9aa7882fab921f
taishi-matsumura <taishi0706@gmail.com>
2020-07-09 09:55:17 +0900

    連打ゲームを作成

--- DirectX2DLibraryCpp/Src/Main.cpp
+++ DirectX2DLibraryCpp/Src/Main.cpp
@@ -8,6 +8,9 @@
 Vec2 g_Position = Vec2(0.0f, 0.0f);
 Vec2 g_Scale = Vec2(1.0f, 1.0f);
 float g_Angle = 0.0f;
+DWORD g_StartTime = 0;
+DWORD g_ElapsedTime = 0;
+int g_MashCount = 0;
 
 // ゲーム処理
 void GameProcessing();
@@ -26,6 +29,8 @@ int WINAPI WinMain(
 	{
 		return 0;
 	}
+
+	g_StartTime = timeGetTime();
 
 	while (true)
 	{
@@ -46,7 +51,16 @@ int WINAPI WinMain(
 		}
 		else
 		{
-			// ゲーム処理
+			// 連打ゲームを作る
+			// ・最初にカウントダウンを表示
+			// ・カウントダウンが終わったらキー入力の受付を開始
+			// ・現在何発連打されているかを表示
+			// ・現在何発連打されているかを表示
+			// ・一定時間経過したら終了（終了した旨を表示、連打した回数はそのまま残す）
+
+			g_ElapsedTime = timeGetTime() - g_StartTime;
+
+			// 入力に対する処理
 			GameProcessing();
 
 			// 描画開始
@@ -74,10 +88,13 @@ void GameProcessing()
 	// キーボードの入力取得
 	//========================================================
 
-	//if (Engine::IsKeyboardKeyPushed(DIK_SPACE) == true)
-	//{
-	//	// キーが押された瞬間の処理
-	//}
+	if (3000 <= g_ElapsedTime && g_ElapsedTime < 13000) {
+		if (Engine::IsKeyboardKeyPushed(DIK_Z) == true) {
+			// キーが押された瞬間の処理
+			g_MashCount++;
+
+		}
+	}
 
 	//if (Engine::IsKeyboardKeyHeld(DIK_LEFT) == true)
 	//{
@@ -96,10 +113,26 @@ void DrawProcessing()
 	// 描画処理を実行する場合、必ず最初実行する
 	Engine::StartDrawing(0);
 
-	// フォント描画
-	Engine::DrawFont(0.0f, 0.0f, "FontSize:Small", FontSize::Small, FontColor::White);
-	Engine::DrawFont(0.0f, 30.0f, "FontSize:Regular", FontSize::Regular, FontColor::White);
-	Engine::DrawFont(0.0f, 60.0f, "FontSize:Large", FontSize::Large, FontColor::White);
+	if (g_ElapsedTime < 3000) {
+		int countDownTime = 3 - (g_ElapsedTime / 1000);
+
+		char countDownTimeString[2];
+		sprintf_s(countDownTimeString, 2, "%d", countDownTime);
+		Engine::DrawFont(300.0f, 200.0f, countDownTimeString, FontSize::Large, FontColor::White);
+	} else if (g_ElapsedTime < 4000) {
+		Engine::DrawFont(250.0f, 200.0f, "Start!!", FontSize::Large, FontColor::White);
+	} else if (g_ElapsedTime < 11000) {
+		Engine::DrawFont(250.0f, 200.0f, "がんばれ～", FontSize::Large, FontColor::White);
+	} else if (g_ElapsedTime < 13000) {
+		Engine::DrawFont(250.0f, 200.0f, "あと少し！！", FontSize::Large, FontColor::White);
+	} else {
+		Engine::DrawFont(250.0f, 200.0f, "しゅーりょー", FontSize::Large, FontColor::White);
+	}
+
+	char mashCountString[32];
+	sprintf_s(mashCountString, 32, "連打回数：%d回", g_MashCount);
+	Engine::DrawFont(200.0f, 260.0f, mashCountString, FontSize::Large, FontColor::White);
+
 
 	// 描画終了
 	// 描画処理を終了する場合、必ず最後に実行する
